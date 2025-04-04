9398d87bf0f067241880daa3404b65101bf79f01
Chandra Bose <chandrabosep3112@gmail.com>
2025-04-04 17:39:45 +0800

    feat:wrapper

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Wrapper from "@/components/wrappers/page";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Your App Name',
-  description: 'Your app description',
-  icons: {
-    icon: '/favicon.ico',
-  },
-}
+	title: "Your App Name",
+	description: "Your app description",
+	icons: {
+		icon: "/favicon.ico",
+	},
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Wrapper>{children}</Wrapper>
+			</body>
+		</html>
+	);
 }
